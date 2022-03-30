178ed534f5a490af9406cc9c5a12bb419dad79c1
Hanna Hashemi <hannahhashemi952@gmail.com>
2022-03-30 12:32:41 +0430

    commit 7

--- LisenceGenerator/MainWindow.xaml.cs
+++ LisenceGenerator/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 namespace LisenceGenerator
 {
     /// <summary>
+    
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
